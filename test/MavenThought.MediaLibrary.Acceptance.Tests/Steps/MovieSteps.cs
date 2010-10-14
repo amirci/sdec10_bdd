fb49627fddcf070ca6788461b15bc734cbc554f3
Amir Barylko <amir@barylko.com>
2010-10-14 07:04:57 -0500

    Demo start: The add movie step has no implementation

---
+++ test/MavenThought.MediaLibrary.Acceptance.Tests/Steps/MovieSteps.cs
@@ -0,0 +1,22 @@
+using System;
+using MavenThought.MediaLibrary.Acceptance.Tests.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace MavenThought.MediaLibrary.Acceptance.Tests.Steps
+{
+    /// <summary>
+    /// Steps related to movie
+    /// </summary>
+    [Binding]
+    public class MovieSteps
+    {
+
+        /// <summary>
+        /// Initialize an instance
+        /// </summary>
+        public MovieSteps()
+        {
+        }
+
+    }
+}
